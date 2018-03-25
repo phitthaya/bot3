fd03c9470fe026a4f1f3cf2e6d584c252709844e
phitthaya <37451527+phitthaya@users.noreply.github.com>
2018-03-26 02:04:58 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -13,7 +13,7 @@ $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
 $_msg = $arrJson['events'][0]['message']['text'];
  
  
-$api_key="<MLAB APIKEY>";
+$api_key="<7l94wcp-6Q84WtKmjM7vM99I19E7VAlt>";
 $url = 'https://api.mlab.com/api/1/databases/duckduck/collections/linebot?apiKey='.$api_key.'';
 $json = file_get_contents('https://api.mlab.com/api/1/databases/duckduck/collections/linebot?apiKey='.$api_key.'&q={"question":"'.$_msg.'"}');
 $data = json_decode($json);
